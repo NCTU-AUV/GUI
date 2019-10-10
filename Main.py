555c2241cf1849f01ea3db5ef58b74f38944fb0c
Your Name <eason27271563.me05@nctu.edu.tw>
2019-10-10 23:41:43 +0800

    miulti page

---
+++ Main.py
@@ -0,0 +1,175 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import numpy as np
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+matplotlib.use('TkAgg')
+import Tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import Tkinter as tk
+
+#==========            Make a 3D Arrow             ==============
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
+#=========            function to rotate           ===========
+def eulerAnglesToRotationMatrix(theta) :
+    R_x = np.array([[1,         0,                  0                   ],
+                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
+                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
+                    ])
+         
+         
+                     
+    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
+                    [0,                     1,      0                   ],
+                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
+                    ])
+                 
+    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
+                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])
+                     
+                     
+    R = np.dot(R_z, np.dot( R_y, R_x ))
+ 
+    return R
+def Re_Canvasdraw(Eular_Matrix):
+    global ax
+    global canvas
+    R=eulerAnglesToRotationMatrix(Eular_Matrix)
+    x_axis=np.dot(R,np.array([[1],[0],[0]]))
+    y_axis=np.dot(R,np.array([[0],[1],[0]]))
+    z_axis=np.dot(R,np.array([[0],[0],[0.5]]))
+    ax.cla()
+    ax.plot([0,y_axis[0][0]], [0,y_axis[1][0]], [0,y_axis[2][0]],  'green',label='X_axis(Roll)')
+    a = Arrow3D([0,x_axis[0][0]], [0,x_axis[1][0]], [0,x_axis[2][0]],mutation_scale=20, lw=1, arrowstyle="-|>", color="red")
+    ax.add_artist(a)
+    ax.plot([0,z_axis[0][0]], [0,z_axis[1][0]], [0,z_axis[2][0]], color='blue')
+    ax.set_xlim(-0.6,1)
+    ax.set_ylim(-0.6,1)
+    ax.set_zlim(-0.6,1)
+    ax.legend()
+    canvas.draw()
+def change_Roll(roll):
+    global x
+    x[0]=float(roll)*math.pi/180.
+    Re_Canvasdraw(x)
+def change_Pitch(pitch):
+    global x
+    x[1]=float(pitch)*math.pi/180.
+    Re_Canvasdraw(x)
+def change_Yaw(yaw):
+    global x
+    x[2]=float(yaw)*math.pi/180.
+    Re_Canvasdraw(x)
+
+
+class Page(tk.Frame):
+	def __init__(self, *args, **kwargs):
+		tk.Frame.__init__(self, *args, **kwargs)
+	def show(self):
+		self.lift()
+
+class Page1(Page):
+	def __init__(self, *args, **kwargs):
+		Page.__init__(self, *args, **kwargs)
+		label = tk.Label(self, text="This is page 1")
+		label.pack(side="top", fill="both", expand=True)
+
+class Page2(Page):
+	def __init__(self, *args, **kwargs):
+		Page.__init__(self, *args, **kwargs)
+		label = tk.Label(self, text="This is page 2")
+		label.pack(side="top", fill="both", expand=True)
+
+class Page3(Page):
+	def __init__(self, *args, **kwargs):
+		Page.__init__(self, *args, **kwargs)
+		fig = Figure(figsize=(10, 10), dpi=50) 
+		canvas = FigureCanvasTkAgg(fig, master=self)
+		canvas.draw()
+		x = [0,0,0]    #Eular angle X ,Y,Z
+		global x
+		R=eulerAnglesToRotationMatrix(x)
+		#print(R)
+		x_axis=np.dot(R,np.array([[1],[0],[0]]))
+		y_axis=np.dot(R,np.array([[0],[1],[0]]))
+		z_axis=np.dot(R,np.array([[0],[0],[0.5]]))
+
+		ax = fig.add_subplot(111, projection='3d')
+		global ax
+		ax.plot([0,x_axis[0][0]], [0,x_axis[1][0]], [0,x_axis[2][0]],  'r',label='X_axis(Roll)')
+		a = Arrow3D([0,y_axis[0][0]], [0,y_axis[1][0]], [0,y_axis[2][0]],mutation_scale=3, lw=1, arrowstyle="-|>", color="green")
+		#ax.plot([0,y_axis[0][0]], [0,y_axis[1][0]], [0,y_axis[2][0]], color='green')
+		ax.plot([0,z_axis[0][0]], [0,z_axis[1][0]], [0,z_axis[2][0]], color='blue')
+		ax.add_artist(a)
+		ax.set_xlim(-0.6,1)
+		ax.set_ylim(-0.6,1)
+		ax.set_zlim(-0.6,1)
+		ax.legend()
+		ax.mouse_init()
+		ax.view_init(30, 189)
+		#toolbar = NavigationToolbar2TkAgg(canvas, root)
+		#toolbar.update()
+		canvas.get_tk_widget().place(x=0, y=0)
+		global canvas
+		s_roll = tk.Scale(self,label="row",from_=-90,to=90,orient=tk.HORIZONTAL,length=200,showvalue=1,tickinterval=90,resolution=0.01,command=change_Roll)
+		s_pitch = tk.Scale(self,label="pitch",from_=-90,to=90,orient=tk.HORIZONTAL,length=200,showvalue=1,tickinterval=90,resolution=0.01,command=change_Pitch)
+		s_yaw = tk.Scale(self,label="yaw",from_=-90,to=90,orient=tk.HORIZONTAL,length=200,showvalue=1,tickinterval=90,resolution=0.01,command=change_Yaw)
+		s_roll.set(0)
+		s_pitch.set(0)
+		s_yaw.set(0)
+		s_yaw.pack()
+		s_pitch.pack()
+		s_roll.pack()
+
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = Page1(self)
+        p2 = Page2(self)
+        p3 = Page3(self)
+
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        b1 = tk.Button(buttonframe, text="Page 1", command=p1.lift)
+        b2 = tk.Button(buttonframe, text="Page 2", command=p2.lift)
+        b3 = tk.Button(buttonframe, text="Page 3", command=p3.lift)
+
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+
+        #p1.show()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("1500x1000")
+    root.mainloop()
